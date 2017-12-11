2922d829b1660da1020190830b7c5eed6d880ad6
Olivier BEZAUDIN <obezaudin@pil-media.com>
2017-12-11 11:12:41 -0400

    init

---
+++ app/Controllers/Http/InvitationController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class InvitationController {
+  async index () {
+  }
+
+  async create ({ view }) {
+    return view.render('pages.backoffice.invitation.form')
+  }
+
+  async store () {
+  }
+
+  async show () {
+  }
+
+  async edit () {
+  }
+
+  async update () {
+  }
+
+  async delete () {
+  }
+}
+
+module.exports = InvitationController
---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,30 @@
+'use strict'
+
+class UserController {
+
+  // list user
+  async index () {
+    return "user list"
+  }
+
+  async create () {
+    return "form user create"
+  }
+
+  async store () {
+  }
+
+  async show () {
+  }
+
+  async edit () {
+  }
+
+  async update () {
+  }
+
+  async delete () {
+  }
+}
+
+module.exports = UserController
